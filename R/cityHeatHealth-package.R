fe84c292b23f58005e4cca7d7f68993a232bd665
Chad Milando <44262671+cmilando@users.noreply.github.com>
2025-12-18 05:42:07 -0800

    Initial commit

---
+++ R/cityHeatHealth-package.R
@@ -0,0 +1,18 @@
+#' The 'cityHeatHealth' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name cityHeatHealth-package
+#' @aliases cityHeatHealth
+#' @useDynLib cityHeatHealth, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#' @importFrom rstantools rstan_config
+#' @importFrom RcppParallel RcppParallelLibs
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.7. https://mc-stan.org
+#'
+NULL
